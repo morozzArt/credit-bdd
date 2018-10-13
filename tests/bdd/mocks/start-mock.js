ea508deafd28301d52a63d539d5261c0aa1ceedf
malinink <apmath@malinink.ru>
2018-10-13 21:37:13 +0300

    cs on start-mock

--- tests/bdd/mocks/start-mock.js
+++ tests/bdd/mocks/start-mock.js
@@ -1,6 +1,6 @@
-function(mockName) {
-  var Mock = Java.type('com.intuit.karate.netty.FeatureServer');
-  var file = new java.io.File('tests/bdd/mocks/' + mockName + '.feature');
-  var server = Mock.start(file, 0, false, null);
-  return server;
+function(mockFeatureName) {
+    var mock = Java.type('com.intuit.karate.netty.FeatureServer');
+    var file = new java.io.File('tests/bdd/mocks/' + mockFeatureName + '.feature');
+    var server = mock.start(file, 0, false, null);
+    return server;
 }
